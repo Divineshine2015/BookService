d661502410a08608c13d98859b0519ebce44f4cd
Jon Reed <mr.is@outlook.com>
2015-04-07 16:01:10 -0400

    mod PostBook method

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -99,8 +99,18 @@ namespace BookService.Controllers
 
             db.Books.Add(book);
             await db.SaveChangesAsync();
+            // New code:
+            // Load author name
+            db.Entry(book).Reference(x => x.Author).Load();
 
-            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+            var dto = new BookDTO()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.Author.Name
+            };
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, dto);
         }
 
         // DELETE: api/Books/5
